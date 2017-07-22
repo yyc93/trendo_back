b0d31eb647895b846000bc754fe6e1fd02d8f2a7
Yang <taipeiyang93@gmail.com>
2017-07-22 12:12:27 +0800

    restart when crash, Add Access-Control-Allow-Origin in header

--- index.js
+++ index.js
@@ -1,69 +1,107 @@
 'use strict';
+/*******************************************************/
+// include 
+/** **************************************************** */
 
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var multer = require('multer');
-var fs = require('fs');
-var path = require('path');
-// var methodOverride = require('method-override');
-
-// db for database connections
-require('./server/model/db.js')
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // cb(null, './client/images/uploads')
-    cb(null, 'server/uploads/')
-  },
-  filename: function (req, file, cb) {
-    let extArray = file.mimetype.split("/");
-    let extension = extArray[extArray.length - 1];
-    cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
+var cluster = require("cluster");
+//
+if (cluster.isMaster) {
+  var numCPUs = require("os").cpus().length;
+  for ( var i = 0; i < numCPUs; i += 1) {
+    cluster.schedulingPolicy = "none";
+    cluster.fork();
   }
-})
 
-process.env.PWD = process.cwd()
+  cluster.on('exit',
+    function(worker) {
+        console.log('Worker ' + worker.id + ' died :(');
+        var w = cluster.fork();
+        console.log('Starting Worker ' + w.id + ' :)');
+    });
+}
+else {
+  var express = require('express');
+  var app = express();
+  var bodyParser = require('body-parser');
+  var multer = require('multer');
+  var fs = require('fs');
+  var path = require('path');
+  var cluster = require("cluster");
+  // var methodOverride = require('method-override');
 
-// parse application/vnd.api+json as json
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+  // db for database connections
+  require('./server/model/db.js')
 
-// to retrieve the data from body
-app.use(bodyParser.urlencoded({extended: false, limit: '50mb', parameterLimit: 1000000}));
-// // to render the data in json
-app.use(bodyParser.json({limit: '50mb'}));
+  var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // cb(null, './client/images/uploads')
+      cb(null, 'server/uploads/')
+    },
+    filename: function (req, file, cb) {
+      let extArray = file.mimetype.split("/");
+      let extension = extArray[extArray.length - 1];
+      cb(null, file.fieldname + '-' + Date.now()+ '.' +extension)
+    }
+  })
 
-// app.use(multer({dest: './client/images/uploads'}).any());
+  process.env.PWD = process.cwd()
 
-app.use(multer({storage: storage}).any());
+  // parse application/vnd.api+json as json
+  // app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
+  // to retrieve the data from body
+  app.use(bodyParser.urlencoded({extended: false, limit: '50mb', parameterLimit: 1000000}));
+  // // to render the data in json
+  app.use(bodyParser.json({limit: '50mb'}));
 
-// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
-// app.use(methodOverride('X-HTTP-Method-Override')); 
+  // I am currently allowing all origins to access this api.
+  app.all('*',
+    function(req, res, next) {
+        // res.header("Content-Type",
+        //             "application/json");
+        res.header("Access-Control-Allow-Origin",
+                    "*");
+        // res.header("Access-Control-Allow-Headers",
+        //             "X-Requested-With,Content-Type");
+        next();
+    });
 
-// to access client static files
-app.use(express.static('./client'));
+  // app.use(multer({dest: './client/images/uploads'}).any());
 
-// to access server static files/images
-// app.use(express.static('./server/uploads'));
-
-app.use('/server',express.static('./server'));
-// app.use('/listPledge',express.static(process.env.PWD + '/server'));
-
-// use routes.js file after express.static function to serve the client
-// routes ==================================================
-require('./server/routes/routes.js')(app); // configure our routes
-
-
-// start app ===============================================
-// startup our app at http://localhost:8080
-var port = process.env.PORT || 8080;
-app.listen(port, function(){
-	console.log('App Listening on localhost:' + port);
-});               
+  app.use(multer({storage: storage}).any());
 
 
-module.exports = app;
+  // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+  // app.use(methodOverride('X-HTTP-Method-Override')); 
+
+  // to access client static files
+  app.use(express.static('./client'));
+
+  // to access server static files/images
+  // app.use(express.static('./server/uploads'));
+
+  app.use('/server',express.static('./server'));
+  // app.use('/listPledge',express.static(process.env.PWD + '/server'));
+
+  // use routes.js file after express.static function to serve the client
+  // routes ==================================================
+  require('./server/routes/routes.js')(app); // configure our routes
+
+  console.log('---------------------------');
+  console.log('Firing Up Threads: ' + cluster.worker.id);
+  console.log('Firing Up Threads: ' + process.pid);
+  console.log('---------------------------');
+
+  // start app ===============================================
+  // startup our app at http://localhost:8080
+  var port = process.env.PORT || 8080;
+  app.listen(port, function(){
+    console.log('App Listening on localhost:' + port);
+  });               
+
+
+  module.exports = app;
+}
 
 
 
